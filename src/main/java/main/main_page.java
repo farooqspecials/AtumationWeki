ae8b67975d83d4228d16d8ebc51bfdf836fe6503
Farooq Abdullah <farooqspecials@gmail.com>
2023-09-20 00:58:32 +0500

    Automation in action

--- src/main/java/main/main_page.java
+++ src/main/java/main/main_page.java
@@ -1,5 +1,19 @@
 package main;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utilities.utils;
+
 public class main_page {
+	private static By SEARCH_FIELD = By.xpath("//*[@id='searchInput']");
+	private static By SEARCH_BUTTON = By.xpath("//*[@id=\"search-form\"]/fieldset/button");
+	
+	public static void testSearch(String searchStr){
+		WebElement searchField = utils.driver.findElement(SEARCH_FIELD);
+		
+		searchField.sendKeys(searchStr);
+		utils.driver.findElement(SEARCH_BUTTON).click();
+	}
 
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,17 @@
+import login.login_page;
+import main.main_page;
+import utilities.utils;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello my friend");
+		utils.instantiate();
+		main_page.testSearch("spider man");
+		login_page.clickLogin();
+		login_page.setUsernameAndPassword();
+		utils.closedriver();
+		
 	}
 
 }
