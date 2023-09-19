208bc3837ae0207574e5b47fcd36c952ca305f1c
Farooq Abdullah <farooqspecials@gmail.com>
2023-09-19 23:16:15 +0500

    setting up web driver

--- src/main/java/utilities/utils.java
+++ src/main/java/utilities/utils.java
@@ -1,5 +1,31 @@
 package utilities;
 
-public class utils {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class utils {
+	
+	public static WebDriver driver;
+	
+	public static WebDriver instantiate() {
+		driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", "./"+"\\Drivers\\geckodriver.exe");
+			
+		
+		//wait for the  elements to appear/load in the web application to avoid exception in applicaiton
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+        driver.manage().window().maximize();
+        driver.get("http://www.wikipedia.org"); 
+		return driver;
+	}
+	
+	public static void closedriver() {
+							
+			driver.close();
+
+
+				}
 }
